de8a565d6cfcb90b146b90c1cfaa9cc3d4206511
Edmundo Gomez <edmundo.gomez5558@gmail.com>
2023-05-02 02:23:05 -0600

    Cross Origin Desactivate

--- src/main/java/com/pirata/rest/RestApplication.java
+++ src/main/java/com/pirata/rest/RestApplication.java
@@ -2,6 +2,9 @@ package com.pirata.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApplication {
@@ -10,4 +13,13 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedMethods("*");
+			}	
+		};
+	}
 }
